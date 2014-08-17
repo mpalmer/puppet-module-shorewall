9c26163353382fee38ef43efbba0d7da7c72bde6
Matt Palmer <mpalmer@hezmatt.org>
2014-08-18 09:50:19 +1000

    Well, that's more than slightly irritating

--- lib/puppet/parser/functions/contains_v4.rb
+++ lib/puppet/parser/functions/contains_v4.rb
@@ -2,10 +2,10 @@ module Puppet::Parser::Functions
 	newfunction(:contains_v4, :type => :rvalue, :doc => "Returns true iff the variable passed to this function is a string which contains something that looks like an IPv4 address or CIDR network") do |args|
 		args.length == 1 or raise Puppet::ParseError.new("contains_v4: expecting 1 argument, got #{args.length}")
 
-		item = args[0]
+		item = args[0].dup
 		item.is_a?(String) or return false
 		# Strip off the leading zone identifier
-		item.gsub!(/^[^:]+/, '')
+		item.gsub!(/^[^:]+:/, '')
 		!!(item =~ %r{\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(/\d{1,2})?})
 	end
 end
--- lib/puppet/parser/functions/contains_v6.rb
+++ lib/puppet/parser/functions/contains_v6.rb
@@ -2,10 +2,10 @@ module Puppet::Parser::Functions
 	newfunction(:contains_v6, :type => :rvalue, :doc => "Returns true iff the variable passed to this function is a string which contains something that looks like an IPv6 address or CIDR network") do |args|
 		args.length == 1 or raise Puppet::ParseError.new("is_v6: expecting 1 argument, got #{args.length}")
 
-		item = args[0]
+		item = args[0].dup
 		item.is_a?(String) or return false
 		# Strip off the leading zone identifier
-		item.gsub!(/^[^:]+/, '')
+		item.gsub!(/^[^:]+:/, '')
 		!!(item =~ %r{[0-9a-fA-F:]+:[0-9a-fA-F:]+(/\d{1,3})?})
 	end
 end
